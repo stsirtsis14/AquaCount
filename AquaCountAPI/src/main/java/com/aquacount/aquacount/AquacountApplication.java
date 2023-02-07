789667c423ab4d5a3709c79d8432f6a89f5e99eb
stsirtsis <spiros.ts7@gmail.com>
2023-02-07 22:28:44 +0200

    spring api init

---
+++ AquaCountAPI/src/main/java/com/aquacount/aquacount/AquacountApplication.java
@@ -0,0 +1,13 @@
+package com.aquacount.aquacount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquacountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquacountApplication.class, args);
+	}
+
+}
